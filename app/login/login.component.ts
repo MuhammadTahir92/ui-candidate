acf766ab0026a7f05f92cabbc1e0c74cfac721c0
Zeeshan Aslam <zeeshan.sulehria@gmail.com>
2018-05-16 09:17:01 -0700

    project running

---
+++ app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Page } from "ui/page";
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  moduleId:module.id
+})
+export class LoginComponent implements OnInit {
+
+  constructor(private _page: Page) { }
+
+  ngOnInit() {
+    this._page.actionBarHidden = true;
+  }
+
+}
